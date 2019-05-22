861893a726716a069983c873dd5d934d14903e8d
apollo1986 <teyyubeyyuboglu@gmail.com>
2019-05-22 08:15:53 +0400

    this class created for easy using logger

---
+++ src/com/botanik/logger/Logger.java
@@ -0,0 +1,30 @@
+package com.botanik.logger;
+
+import java.io.IOException;
+import java.util.logging.FileHandler;
+import java.util.logging.Level;
+
+/**
+ *
+ * @author teyyub , 8:05:49 AM
+ */
+public class Logger {
+
+    static final String dir = System.getProperty("user.dir");
+    static final java.util.logging.Logger logger = java.util.logging.Logger.getLogger("Mylog");
+
+    public Logger() {
+
+    }
+
+    public static void save(String message) {
+        try {
+            FileHandler fh = new FileHandler(dir + "/slog.txt");
+            logger.addHandler(fh);
+        } catch (IOException ex) {
+
+        }
+
+        logger.log(Level.INFO, message);
+    }
+}
