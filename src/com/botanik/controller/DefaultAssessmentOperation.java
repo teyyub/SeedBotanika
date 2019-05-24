688c50f524c94ec54d6a09e72993130cf5293ddf
apollo1986 <teyyubeyyuboglu@gmail.com>
2019-05-24 14:31:38 +0400

    search method changed to common load

--- src/com/botanik/controller/DefaultAssessmentOperation.java
+++ src/com/botanik/controller/DefaultAssessmentOperation.java
@@ -5,6 +5,7 @@ import com.botanik.dao.impl.CombosDAOJDBC;
 import com.botanik.dao.intf.CombosDAO;
 import com.botanik.model.Base;
 import com.botanik.model.CollectionDataBase;
+import com.botanik.model.Combo;
 import java.io.IOException;
 import java.util.List;
 import java.util.Optional;
@@ -88,7 +89,8 @@ public class DefaultAssessmentOperation {
         if ((nameText.getText().equals("")) || (nameText.getText().length() == 0)) {
             loadTable();
         } else {
-            assessments = baseDao.familyByName(nameText.getText());
+            Combo c = new Combo(12,nameText.getText());
+            assessments = baseDao.loadByCombo(c);
             assessmentList = FXCollections.observableArrayList(assessments);
             table.setItems(assessmentList);
             table.refresh();
